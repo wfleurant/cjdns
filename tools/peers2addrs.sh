9200d37f26951c11c5230059e431f3eca9c3d641
William Fleurant <william@netblazr.com>
2017-07-17 01:19:29 +0000

    tools: peers to addresses w/ more pingery stuffs

--- tools/peers2addrs.sh
+++ tools/peers2addrs.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-for x in $(./peerStats | cut -d. -f6 ); do
+for x in $(./peerStats | cut -d. -f6); do
 
-  p=$(../publictoip6 ${x}.k )
+  p=$(../publictoip6 ${x}.k)
+  echo "------------------------------------------------------ - ----  ----  ----  ----  ----  ----  ----  ----"
+  echo ${x}.k = ${p} | column -t -s ":"
+  echo "------------------------------------------------------ - ----  ----  ----  ----  ----  ----  ----  ----"
+  echo -en "\n\trtHops "
+  cjdcmd-ng traceroute -r ${p} 2>&1 | grep -v ^Failed
 
-  echo ${x}.k = ${p}
+  echo -e "\n"
+  echo -e "\trtStat "$(./sessionStats | grep ${x}.k | sed "s/\.${x}\.k//1")
   echo -e "\t"$(./sessionStats | grep ${x}.k | sed "s/\.${x}\.k//1")
-  echo -e "\t"$(2>/dev/null ./ping ${p} | head -n1 | sed "s/\.${x}\.k//1" | awk '{print $2,$3}')"\n"
+  echo -e "\n"
+  echo -e "\trtPing "$(2>/dev/null ./ping ${p} | head -n1 | sed "s/\.${x}\.k//1" | awk '{print $2,$3}')"\n"
+  echo -e "\n"
 
 done
 
